0351d386b37b05fbeaa2985c8554465d54cfc66a
Ritvik Jayaswal <rj7650@gmail.com>
2020-02-14 22:23:09 -0500

    Added edit and delete

--- src/main/java/com/uncc/ticket/controller/TicketController.java
+++ src/main/java/com/uncc/ticket/controller/TicketController.java
@@ -1,6 +1,7 @@
 package com.uncc.ticket.controller;
 
 import com.uncc.ticket.model.UsersEntity;
+import org.springframework.beans.PropertyEditorRegistry;
 import org.springframework.validation.BindingResult;
 import com.uncc.ticket.model.TicketEntity;
 import com.uncc.ticket.service.TicketService;
@@ -9,10 +10,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.beans.PropertyEditor;
 import java.security.Principal;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class TicketController {
@@ -53,12 +60,15 @@ public class TicketController {
     @RequestMapping(value = "/tickets/edit/{id}", method = RequestMethod.GET)
     public String editTicket(Model model,@PathVariable("id") Long id) {
         // Code here
-        return "redirect:/"; //Remove this line
+        TicketEntity hi = ticketService.findById(id);
+        model.addAttribute("ticket", hi);
+        return "tickets/storeTicket";
     }
 
     @RequestMapping(value = "/tickets/delete/{id}", method = RequestMethod.GET)
     public String deleteTicket(@PathVariable("id") Long id) {
         // Code here
+        ticketService.deleteById(id);
         return "redirect:/";
     }
 
